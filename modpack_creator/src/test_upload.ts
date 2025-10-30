bdf10839d00889fe669db0cc4e0acd7da858a5cf
ViktorTrojan <viktor.trojann@gmail.com>
2025-10-31 00:29:53 +0100

    test update

--- modpack_creator/src/test_upload.ts
+++ modpack_creator/src/test_upload.ts
@@ -1,8 +1,12 @@
+import { resolve } from "node:path"
 import { config } from "./config"
 import { upload_to_modrinth } from "./upload_to_modrinth"
 
+const root_dir = resolve(__dirname, "../..")
+const path = await resolve(root_dir, "Sodium Vanilla-1.21.10_0.1.6_full.mrpack")
+
 const result = await upload_to_modrinth({
-  file_path: "/app/Sodium Vanilla-0.1.0.mrpack",
+  file_path: path,
   version_number: `${config.app.mc_version}_safe`,
   version_title: `Sodium Vanilla ${config.app.mc_version} (Safe)`,
   changelog: `Safe to use version for servers for Minecraft ${config.app.mc_version}`,
